f558f95de89037016b75638d5606ac6a206efd72
V2Mini <openrobotics.v2.mini@gmail.com>
2016-10-16 14:39:10 -0700

    started motor firmware

--- v2mini_init/firmware/unoware.cpp
+++ v2mini_init/firmware/unoware.cpp
@@ -1,55 +1,174 @@
 #include <Arduino.h>
-
 #include <ros.h>
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/Float32.h"
 
 ros::NodeHandle nh;
 
-// arduino vars
-int out1 = 13;
+const float pi = 3.1416;
 
-// ros vars
-int velX, velY, velAngular, torso_height;
+const int max_speed = 1000;         // rps ...update val
+const int wheel_radius = 4;         // cm
+const int base_radius = 16;         // cm
+const int accel_delay = 1;         // s/bit (~3.8s for full speed)
+// NO CAN'T ACCEL LIKE THIS...ITS JUST BLOCKING...
+
+// Front-Right base motor.
+int motorFR_pwm = 3;
+int motorFR_d1 = 13;
+int motorFR_d2 = 4;
+
+// Front-Left base motor.
+int motorFL_pwm = 5;
+int motorFL_d1 = 7;
+int motorFL_d2 = 10;
+
+// Back-Right base motor.
+int motorBR_pwm = 6;
+int motorBR_d1 = 10;
+int motorBR_d2 = 10;
+
+// Back-Left base motor.
+int motorBL_pwm = 9;
+int motorBL_d1 = 10;
+int motorBL_d2 = 10;
+
+// Only need to calculate motor speeds for FR & FL.
+float motor_vectorFR[] = {-0.7071, 0.7071, 1};
+float motor_vectorFL[] = {-0.7071, -0.7071, 1};
+
+float motorFR_speed, motorFL_speed;
+int velX, velY, velW, torso_height;
+bool cwFR, cwFL;
 
 void motion_cb(const geometry_msgs::Twist& motion_cmds) {
   velX = motion_cmds.linear.x;
   velY = motion_cmds.linear.y;
-  velAngular = motion_cmds.angular.z;
+  velW = motion_cmds.angular.z;
   torso_height = motion_cmds.linear.z;
 }
 
+// Subscribe to ROS topic "/base_cmds"
 ros::Subscriber<geometry_msgs::Twist> sub_motion(
   "base_cmds", &motion_cb);
 
+// --- FOR TESTING
+std_msgs::Float32 float_msg;
+ros::Publisher debugger("calcs", &float_msg);
+
 void move_base() {
 
-  digitalWrite(out1, LOW);
-  delay(velY*1000);
-  digitalWrite(out1, HIGH);
-  delay(velY*1000);
+  // Calculate motor speeds.
+  motorFR_speed =
+  motor_vectorFR[0] * velX + motor_vectorFR[1] * velY +
+  motor_vectorFR[2] * velW * pi / 180 * base_radius;
+
+  motorFL_speed =
+  motor_vectorFL[0] * velX + motor_vectorFL[1] * velY +
+  motor_vectorFL[2] * velW * pi / 180 * base_radius;
+
+  float_msg.data = motorFL_speed;
+
+  // Motor direction.
+  cwFR = motorFR_speed < 0 ? true : false;
+  cwFL = motorFL_speed < 0 ? true : false;
+
+  motorFR_speed = abs(motorFR_speed);
+  motorFL_speed = abs(motorFL_speed);
+
+  // Motor Speeds are capped so preserve speed ratio.
+  if (motorFR_speed >= motorFL_speed && motorFR_speed > max_speed) {
+    motorFL_speed = motorFL_speed * max_speed / motorFR_speed;
+    motorFR_speed = max_speed;
+
+  } else if (motorFL_speed >= motorFR_speed && motorFL_speed > max_speed) {
+    motorFR_speed = motorFR_speed * max_speed / motorFL_speed;
+    motorFL_speed = max_speed;
+  }
+
+  // Convert speeds to 8-bit.
+  motorFR_speed = map(motorFR_speed, 0, max_speed, 0, 255);
+  motorFL_speed = map(motorFL_speed, 0, max_speed, 0, 255);
+
+  // Set motor direction.
+  if (cwFR == true) {
+    digitalWrite(motorFR_d1, LOW);
+    digitalWrite(motorFR_d2, HIGH);
+    digitalWrite(motorBL_d1, HIGH);
+    digitalWrite(motorBL_d2, LOW);
+  } else {
+    digitalWrite(motorFR_d1, HIGH);
+    digitalWrite(motorFR_d2, LOW);
+    digitalWrite(motorBL_d1, LOW);
+    digitalWrite(motorBL_d2, HIGH);
+  }
+
+  if (cwFL == true) {
+    digitalWrite(motorFL_d1, LOW);
+    digitalWrite(motorFL_d2, HIGH);
+    digitalWrite(motorBR_d1, HIGH);
+    digitalWrite(motorBR_d2, LOW);
+  } else {
+    digitalWrite(motorFL_d1, HIGH);
+    digitalWrite(motorFL_d2, LOW);
+    digitalWrite(motorBR_d1, LOW);
+    digitalWrite(motorBR_d2, HIGH);
+  }
+
+  // Accelerate motors.
+  // --> may need varying rates..
+  for (int i = 0; i < 255; i++) {
+
+    if (motorFR_speed >= i) {
+      analogWrite(motorFR_pwm, i);
+      analogWrite(motorBL_pwm, i);
+    }
+    if (motorFL_speed >= i) {
+      analogWrite(motorFL_pwm, i);
+      analogWrite(motorBR_pwm, i);
+    }
+    delay(accel_delay);
+  }
 }
 
 void move_torso() {
+
 }
 
 void setup() {
 
-  // arduino setup
-  pinMode(out1, OUTPUT);
+  pinMode(motorFR_pwm, OUTPUT);
+  pinMode(motorFR_d1, OUTPUT);
+  pinMode(motorFR_d2, OUTPUT);
+
+  pinMode(motorFL_pwm, OUTPUT);
+  pinMode(motorFL_d1, OUTPUT);
+  pinMode(motorFL_d2, OUTPUT);
+
+  pinMode(motorBR_pwm, OUTPUT);
+  pinMode(motorBR_d1, OUTPUT);
+  pinMode(motorBR_d2, OUTPUT);
+
+  pinMode(motorBL_pwm, OUTPUT);
+  pinMode(motorBL_d1, OUTPUT);
+  pinMode(motorBL_d2, OUTPUT);
 
   // ros setup
   nh.initNode();
   nh.subscribe(sub_motion);
 
+  // --- FOR TESTING
+  nh.advertise(debugger);
+
 }
 
 void loop() {
 
-  // State 1: Move the base ---
-  move_base();
+  // State 1: Control the base ---
+  move_base(); // TODO will need to pass error adjustments as arg...
 
-
-  // State 2: Move the torso ---
+  // State 2: Control the torso ---
+  move_torso();
 
   // State 3: Measure the batteries ---
 
@@ -57,6 +176,7 @@ void loop() {
 
   // State 5: Calculate base error ---
 
+  debugger.publish(&float_msg);
   nh.spinOnce();
   delay(1);
 }
--- v2mini_motion/src/motion_control.cpp
+++ v2mini_motion/src/motion_control.cpp
@@ -6,67 +6,69 @@
 #include <ncurses.h>
 
 /**
- * Control movements of the base and torso autonomously or remotely.
+ * Control movements of the base and torso autonomously or with a UI.
  */
 
-class Remote_Control {
+class Robot_UI {
 
 private:
   ros::NodeHandle n;
   ros::Publisher base_pub;
 
 public:
-  Remote_Control(ros::NodeHandle &nh) {
+  Robot_UI(ros::NodeHandle &nh) {
     n = nh;
     base_pub = n.advertise<geometry_msgs::Twist>("motion_cmds", 1);
   }
 
   void keyboard_interface() {
 
-    ros::Rate loop_rate(50);
+    ros::Rate loop_rate(20);
 
+    // Limitation of ncurses - can only read a single key at a time ..
     char key_cmd;
     initscr();
+    noecho();
     cbreak();
-    timeout(100);
+    nodelay(stdscr,TRUE);
 
     while(ros::ok()){
 
       geometry_msgs::Twist motion_cmds;
 
-      key_cmd = getch();  // update to read multiple keys concurrently
+      key_cmd = getch();
 
         if(key_cmd == 'w') {
           // set y-velocity -> forwards
-          motion_cmds.linear.y = 1;
+          motion_cmds.linear.y = 1000;
 
         } else if(key_cmd == 's') {
           // set y-velocity -> backwards
-          motion_cmds.linear.y = -1;
+          motion_cmds.linear.y = -1000;
 
         } else if(key_cmd == 'd') {
           // set x-velocity -> right
-          motion_cmds.linear.x = 1;
+          motion_cmds.linear.x = 1000;
 
         } else if(key_cmd == 'a') {
           // set x-velocity -> left
-          motion_cmds.linear.x = -1;
+          motion_cmds.linear.x = -1000;
 
         } else if(key_cmd == 'i') {
           // set angular velocity -> CCW
-          motion_cmds.angular.z = 1;
+          motion_cmds.angular.z = 1000;
 
         } else if(key_cmd == 'o') {
           // set angular velocity -> CW
-          motion_cmds.angular.z = -1;
+          motion_cmds.angular.z = -1000;
 
         } else if(key_cmd == 'r') {
           // set delta torso height -> up
-          motion_cmds.linear.z = 1;
+          motion_cmds.linear.z = 1000;
 
         } else if(key_cmd == 'f') {
           // set delta torso height -> down
-          motion_cmds.linear.z = -1;
+          motion_cmds.linear.z = -1000;
 
         } else if(key_cmd == 'b') {
           break;
@@ -94,7 +96,7 @@ int main(int argc, char** argv)
   ROS_DEBUG_STREAM("control type:" << control_type);
 
   if (control_type == "remote"){
-    Remote_Control robocontrol(nh);
+    Robot_UI robocontrol(nh);
     robocontrol.keyboard_interface();
 
   } else {
