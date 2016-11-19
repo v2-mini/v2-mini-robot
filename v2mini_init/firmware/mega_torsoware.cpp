eef6817e543c94c1258b9ad63c8158cdab4295b1
V2Mini <openrobotics.v2.mini@gmail.com>
2016-11-19 14:26:41 -0800

    simple face toggler

--- v2mini_init/firmware/mega_torsoware.cpp
+++ v2mini_init/firmware/mega_torsoware.cpp
@@ -4,33 +4,70 @@
 
 ros::NodeHandle nh;
 
-const int TORSO_D = 8;
+const int RED = 44;
+const int GREEN = 45;
+const int BLUE = 46;
 
-void motion_cb(const geometry_msgs::Twist& motion_cmds) {
-  // msg type to use? probably don't need twist
+int curr_color = RED;
+int face_input = 0;
+
+void motion_cb(const geometry_msgs::Twist& motion_cmds)
+{
+  // todo msg type to use? probably don't need twist
+  face_input = motion_cmds.linear.x;
 }
 
 // Subscribe to ROS topic "/torso_cmds"
 ros::Subscriber<geometry_msgs::Twist> sub_motion(
   "torso_cmds", &motion_cb);
 
-void move_torso() {
+void move_torso()
+{
   return;
 }
 
-void setup() {
+void toggle_face()
+{
+  if (face_input != 0)
+  {
+    if (curr_color == RED)
+    {
+      digitalWrite(curr_color, LOW);
+      digitalWrite(GREEN, HIGH);
+      curr_color = GREEN;
+    }
+    else if (curr_color == GREEN)
+    {
+      digitalWrite(curr_color, LOW);
+      digitalWrite(BLUE, HIGH);
+      curr_color = BLUE;
+    }
+    else
+    {
+      digitalWrite(curr_color, LOW);
+      digitalWrite(RED, HIGH);
+      curr_color = RED;
+    }
+  }
+}
 
+void setup()
+{
   // setup pins
-  pinMode(TORSO_D, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
 
   // setup ros
   nh.initNode();
   nh.subscribe(sub_motion);
 }
 
-void loop() {
-
+void loop()
+{
   move_torso();
+
+  toggle_face();
 
   nh.spinOnce();
 }
