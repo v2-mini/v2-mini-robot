954be9b83b3de5678fcc47f3e0d2946219d3e429
jon <jmoore90@hotmail.ca>
2016-12-04 11:54:20 -0800

    added missing CMakeLists

--- v2mini_teleop/src/teleop.cpp
+++ v2mini_teleop/src/teleop.cpp
@@ -27,7 +27,7 @@ int main(int argc, char ** argv) {
     const double degree = M_PI/180;
 
     // robot state
-    double tilt = 0, tinc = degree, swivel=0, angle=0, height=0, hinc=0.005;
+    double angle = 0, tilt = 0, tinc = 0;
 
 	// Get type of control from param (auto or remote)
 	std::string controller_type;
@@ -59,34 +59,24 @@ int main(int argc, char ** argv) {
 		geometry_msgs::Twist base_cmds;
 		geometry_msgs::Twist torso_cmds;
 
-		//update joint_state
-		joint_state.header.stamp = ros::Time::now();
-		joint_state.name.resize(3);
-		joint_state.position.resize(3);
-		joint_state.name[0] ="swivel";
-		joint_state.position[0] = swivel;
-		joint_state.name[1] ="tilt";
-		joint_state.position[1] = tilt;
-		joint_state.name[2] ="periscope";
-		joint_state.position[2] = height;
+		 //update joint_state
+		 joint_state.header.stamp = ros::Time::now();
+		 joint_state.name.resize(1);
+		 joint_state.position.resize(1);
+		 joint_state.name[0] ="torso_slide";
+		 joint_state.position[0] = tinc;
 
 		// update transform
 		// (moving in a circle with radius=2)
 		odom_trans.header.stamp = ros::Time::now();
 		odom_trans.transform.translation.x = cos(angle)*2;
 		odom_trans.transform.translation.y = sin(angle)*2;
-		odom_trans.transform.translation.z = .7;
 		odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(angle+M_PI/2);
 
 		joint_pub.publish(joint_state);
 		broadcaster.sendTransform(odom_trans);
 
-		// Create new robot state
-		tilt += tinc;
-		if (tilt<-.5 || tilt>0) tinc *= -1;
-		height += hinc;
-		if (height>.2 || height<0) hinc *= -1;
-		swivel += degree;
+		// // Create new robot state
 		angle += degree/4;
 
 		while(SDL_PollEvent(&event) != 0)
